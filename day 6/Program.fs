440c48e47918a478cf5448aa3c28787c37de59fa
VanillaBrooks <brookskarlik@gmail.com>
2019-12-19 05:00:45 -0800

    cleanup

--- day 6/Program.fs
+++ day 6/Program.fs
@@ -36,36 +36,25 @@ let rec pathFind (startOrbit: ParsedOrbit) (allOrbits: ParsedOrbit []) =
 
 
 let checkPath startOrbit destOrbit (prevOrbit: List<ParsedOrbit>) = 
-    // printfn "checking \n%A\n against \n%A\n with previous \n%A\n" startOrbit destOrbit prevOrbit
-    let res = 
-        if startOrbit.parent = destOrbit.child && not (List.exists (fun x-> x = destOrbit) prevOrbit) then true
-        elif startOrbit.child = destOrbit.parent && not (List.exists (fun x-> x = destOrbit) prevOrbit) then true
-        else false
-    // printfn "%A" res
-    res
+    if startOrbit.parent = destOrbit.child && not (List.exists (fun x-> x = destOrbit) prevOrbit) then true
+    elif startOrbit.child = destOrbit.parent && not (List.exists (fun x-> x = destOrbit) prevOrbit) then true
+    else false
 
 let rec pathFindPoints (startOrbit: ParsedOrbit) (destOrbit: ParsedOrbit) (prevOrbit: List<ParsedOrbit>) (allOrbits: ParsedOrbit []) (depth: int) = 
     let d2 = depth + 1
-    if startOrbit.child = destOrbit.parent then 
-        // printfn "AT destination returning  out with depth %i" depth
-        // printfn "START:\n%A\nDEST:\n%A\nPREV:\n%A\n" startOrbit destOrbit prevOrbit
-        depth
+    if startOrbit = destOrbit then depth
     else 
-
-        let mutable depth = 0;
-        let candidates : ParsedOrbit [] = 
-            allOrbits 
-            |> Array.filter (fun orbit -> checkPath startOrbit orbit prevOrbit)
-
-        // TODO refactor into for pipes
-        for orbit in candidates do
-            let previousOrbits = startOrbit::prevOrbit
-            let temp = pathFindPoints orbit destOrbit previousOrbits allOrbits d2
-
-            if temp <> 0 then depth <- temp
+        let mutable depth = 0
+        allOrbits 
+        // find all valid & non-visited paths
+        |> Array.filter (fun orbit -> checkPath startOrbit orbit prevOrbit)
+        // recursively check each possible path
+        |> Array.map (fun orbit -> pathFindPoints orbit destOrbit (startOrbit::prevOrbit) allOrbits d2)
+        // set the depth equal to a value that is not zero
+        |> Array.map (fun x -> if x <> 0 then depth <- x)
+        |> ignore
 
         depth
-
 
 let getItem (name: Planet) (orbits: ParsedOrbit []) = 
     let x : ParsedOrbit [] = orbits |> Array.filter (fun orbits -> orbits.child = name)
@@ -80,7 +69,6 @@ let testCases =
     let f : ParsedOrbit= getItem "F" orbitData
     let b : ParsedOrbit= getItem "B" orbitData
 
-    start dest prev
     assert (checkPath e d [d] = false) // dest same as previous
     assert (checkPath e d [f] = true)     // dest diff than previous
     assert (checkPath e b [f] = false)    // dest not reachable
